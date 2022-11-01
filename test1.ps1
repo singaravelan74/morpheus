5a42ae64712b268af4423ffe0ddb4530084e2192
Singaravelan <singaravelan74@yahoo.com>
2022-11-01 14:14:42 +0530

    Update test1.ps1

--- test1.ps1
+++ test1.ps1
@@ -1,1 +1,12 @@
 write-output "Hello World test1"
+$var = "<%=results.fn68355_1%>"
+
+write-output "task1 result is $var"
+if($var.trim() -eq "yes")
+{
+	"previos result is yes"
+}
+else
+{
+	"no previous result returned"
+}
